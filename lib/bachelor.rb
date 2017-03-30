88a00ac5660ea0758e4cbe9629d6c96561699380
Eric Farber <eric.j.farber@gmail.com>
2017-03-30 00:07:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,31 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    winner = ""
+    data[season].each do |stats, value|
+        if stats["status"] == "Winner"
+            winner = stats["name"].split(" ")[0]
+        end
+    end
+    winner
 end
+
+
+
 
 def get_contestant_name(data, occupation)
-  # code here
+    name = ""
+        data.each do |season, person|
+            person.each do |stats, value|
+                if stats["occupation"] == occupation
+                    name = stats["name"]
+                end
+            end
+        end
+        name
 end
 
-def count_contestants_by_hometown(data, hometown)
+def count_contestants_by_hometown(data, hometown)exit
   # code here
 end
 
